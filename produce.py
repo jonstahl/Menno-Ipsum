e3856867fde2fb3dc11b604abee68ae2d1edaffd
David Glick <dglick@gmail.com>
2011-10-07 01:12:01 -0700

    add my experiments with using a context-free grammar

---
+++ produce.py
@@ -0,0 +1,43 @@
+from nltk import parse_cfg, ChartParser
+from random import choice
+
+def produce(grammar, symbol):
+    words = []
+    productions = grammar.productions(lhs = symbol)
+    production = choice(productions)
+    for sym in production.rhs():
+        if isinstance(sym, str):
+            words.append(sym)
+        else:
+            words.extend(produce(grammar, sym))
+    return words
+
+grammar = parse_cfg('''
+S -> NP VP
+PP -> P NP_no_PP | 'to get more billable hours' | 'in our wheelhouse' | 'in the bullpen' | 'in the cloud' | 'in the wiki' | 'in the wild'
+NP -> Det N | Det AP N | Det N PP | N_plural | AP N_plural | AP N_plural PP | N_proper
+NP_no_PP -> Det N | Det AP N | N_plural | AP N_plural | N_proper
+VP -> Vtrans NP | Vintrans | VP PP
+Vintrans -> 'iterates' | 'adds value' | 'innovates' | 'sucks less' | 'is connected'
+Vtrans -> 'leverages' | 'creates' | 'pings' | 'postmortems' | 'refactors' | 'dogfoods' | 'asymptotically approaches'
+AP -> AP | A
+Det -> 'a' | 'the' | 'every'
+Det_plural -> 'some' | 'all' | 'most'
+A -> 'suboptimal' | 'nontrivial' | 'scrummy' | 'granular' | 'batshit crazy' | 'nonprofit' | 'open source' | 'performant'
+N_plural -> 'analytics' | 'best practices' | 'strategies' | 'engagement superpowers'
+N_proper -> 'I' | 'Gideon Rosenblatt' | 'Little House on the Prairie' | 'Groundwire' | 'Neal Myrick' | 'Plone' | 'Salesforce' | 'Skype' | 'Rally' | 'an agile process' | 'a bunch of technobabbling punks' | 'grooming the backlog' | 'the environmental movement' | 'the full meal deal' | 'what good looks like'
+N -> 'theory of change' | 'sprint' | 'wordle' | 'bucket' | 'campaign' | 'low-hanging fruit' | 'sprint planning' | 'technology' | 'user story' | 'engagement' | 'movement as network' | 'social change' | 'data migration' | 'innovation' | 'capacity building' | 'theming'
+P -> 'in' | 'outside' | 'to' | 'on' | 'about'
+''')
+
+parser = ChartParser(grammar)
+gr = parser.grammar()
+
+def build_sentence():
+    words = produce(gr, gr.start())
+    sentence = ' '.join(words) + '.'
+    sentence = sentence[0].upper() + sentence[1:]
+    return sentence
+
+for i in range(10):
+    print build_sentence()
