1045b67a8e6c2e60575356fea0855e7c736e20d3
Jon Stahl <jonstahl@gmail.com>
2011-10-07 19:54:27 -0700

    Words, words, words

--- produce.py
+++ produce.py
@@ -15,20 +15,20 @@ def produce(grammar, symbol):
 grammar = parse_cfg('''
 S_full -> S | SubConj S ',' S
 S -> NP VP
-PP -> P NP_no_PP | 'to get more billable hours' | 'in our wheelhouse' | 'in the bullpen' | 'in the cloud' | 'in the wiki' | 'in the wild'
+PP -> P NP_no_PP | 'to get more billable hours' | 'in our wheelhouse' | 'in the bullpen' | 'in the cloud' | 'in the wiki' | 'in the wild' | 'in the streets' | 'with conviction'
 NP -> Det N | Det AP N | Det N PP | N_plural | AP N_plural | AP N_plural PP | N_proper
 NP_no_PP -> Det N | Det AP N | N_plural | AP N_plural | N_proper
 VP -> Vtrans NP | Vintrans | VP PP
-Vintrans -> 'iterates' | 'adds value' | 'innovates' | 'sucks less' | 'is connected'
-Vtrans -> 'leverages' | 'creates' | 'pings' | 'postmortems' | 'refactors' | 'dogfoods' | 'asymptotically approaches' | 'engages' | 'networks with'
+Vintrans -> 'iterates' | 'adds value' | 'innovates' | 'sucks less' | 'is connected' 
+Vtrans -> 'leverages' | 'creates' | 'pings' | 'postmortems' | 'refactors' | 'dogfoods' | 'asymptotically approaches' | 'engages' | 'networks with' | 'organizes' 
 AP -> AP | A
 Det -> 'a' | 'the' | 'every'
 Det_plural -> 'some' | 'all' | 'most'
-A -> 'suboptimal' | 'nontrivial' | 'scrummy' | 'granular' | 'batshit crazy' | 'nonprofit' | 'open source' | 'performant'
-N_plural -> 'analytics' | 'best practices' | 'strategies' | 'engagement superpowers' | 'API limits'
-N_proper -> 'I' | 'Gideon Rosenblatt' | 'Little House on the Prairie' | 'Groundwire' | 'Neal Myrick' | 'Plone' | 'Salesforce' | 'Skype' | 'Rally' | 'an agile process' | 'a bunch of technobabbling punks' | 'grooming the backlog' | 'the environmental movement' | 'the full meal deal' | 'what good looks like' | 'HQ' | 'Jon Stahl' | 'Groundwire Labs'
-N -> 'theory of change' | 'sprint' | 'wordle' | 'bucket' | 'campaign' | 'low-hanging fruit' | 'sprint planning' | 'technology' | 'user story' | 'engagement' | 'movement as network' | 'social change' | 'data migration' | 'innovation' | 'capacity building' | 'theming' | 'advanced functionality' | 'Engagement Pyramid' | 'Gantt chart' | 'scope' | 'scrumbucket' | 'consulting' | 'engagement level' | 'utilization rate' | 'Chipotle order' | 'penguin' | 'line of sight' | 'situational awareness'
-P -> 'in' | 'outside' | 'to' | 'on' | 'about'
+A -> 'suboptimal' | 'nontrivial' | 'scrummy' | 'granular' | 'batshit crazy' | 'nonprofit' | 'open source' | 'performant' | 'hybrid' 
+N_plural -> 'analytics' | 'best practices' | 'strategies' | 'engagement superpowers' | 'API limits' | 'funders' | 'audiences' | 'supporters' | 'members'
+N_proper -> 'I' | 'Gideon Rosenblatt' | 'Wilco' | 'Little House on the Prairie' | 'Groundwire' | 'Neal Myrick' | 'Plone' | 'Salesforce' | 'Skype' | 'Rally' | 'an agile process' | 'a bunch of technobabbling punks' | 'grooming the backlog' | 'the environmental movement' | 'the full meal deal' | 'what good looks like' | 'HQ' | 'Jon Stahl' | 'Groundwire Labs'
+N -> 'bandwidth' | 'theory of change' | 'sprint' | 'wordle' | 'bucket' | 'campaign' | 'low-hanging fruit' | 'sprint planning' | 'technology' | 'user story' | 'engagement' | 'movement as network' | 'social change' | 'data migration' | 'innovation' | 'capacity building' | 'theming' | 'advanced functionality' | 'Engagement Pyramid' | 'Gantt chart' | 'scope' | 'scrumbucket' | 'consulting' | 'engagement level' | 'utilization rate' | 'Chipotle order' | 'penguin' | 'line of sight' | 'situational awareness'
+P -> 'in' | 'outside' | 'to' | 'on' | 'about' | 'around'
 SubConj -> 'although' | 'because' | 'while' | 'after' | 'as' | 'before' | 'if' | 'as long as' | 'since' | 'though' | 'unless' | 'whenever'
 ''')
 
