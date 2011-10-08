0c13551abb4b95228bc0c08218adcd35229908a8
David Glick <dglick@gmail.com>
2011-10-07 23:57:17 -0700

    break out collective nouns

--- produce.py
+++ produce.py
@@ -16,7 +16,7 @@ grammar = parse_cfg('''
 S_full -> S | SubConj S ',' S
 S -> NP VP | NP_plural VP_plural
 PP -> P NP | 'to get more billable hours' | 'in our wheelhouse' | 'in the bullpen' | 'in the cloud' | 'in the wiki' | 'in the wild' | 'in the streets' | 'with conviction'
-NP        -> Det N               | Det AP N               | Det N PP               | N_proper
+NP        -> Det N | Det AP N | Det N PP | N_proper | N_collective | Det_plural N_collective | AP N_collective | Det_plural AP N_collective | N_collective PP | Det_plural N_collective PP
 NP_plural -> Det_plural N_plural | Det_plural AP N_plural | Det_plural N_plural PP | N_plural | AP N_plural | AP N_plural PP
 VP        -> Vtrans NP | Vintrans | VP PP
 VP_plural -> Vtrans_plural NP | Vintrans_plural | VP_plural PP
@@ -26,12 +26,13 @@ Vtrans        -> 'leverages' | 'creates' | 'pings' | 'postmortems' | 'refactors'
 Vtrans_plural -> 'leverage'  | 'create'  | 'ping'  | 'postmortem'  | 'refactor'  | 'dogfood'  | 'asymptotically approach'   | 'engage'  | 'network with'  | 'organize'
 AP -> AP | A
 Det -> 'a' | 'the' | 'every'
-Det_plural -> 'some' | 'all' | 'most'
+Det_plural -> 'the' | 'some' | 'all' | 'most'
 A -> 'suboptimal' | 'tactical' | 'global' | 'nontrivial' | 'scrummy' | 'granular' | 'batshit crazy' | 'nonprofit' | 'open source' | 'performant' | 'hybrid' 
-N_plural -> 'trees' | 'analytics' | 'metrics' | 'best practices' | 'strategies' | 'engagement superpowers' | 'API limits' | 'funders' | 'audiences' | 'supporters' | 'members'
+N_plural -> 'trees' | 'metrics' | 'best practices' | 'strategies' | 'engagement superpowers' | 'API limits' | 'funders' | 'audiences' | 'supporters' | 'members'
 N_proper -> 'Gideon Rosenblatt' | 'Wilco' | 'Little House on the Prairie' | 'Groundwire' | 'Neal Myrick' | 'Plone' | 'Salesforce' | 'Skype' | 'Rally' | 'an agile process' | 'a bunch of technobabbling punks' | 'grooming the backlog' | 'the environmental movement' | 'the full meal deal' | 'what good looks like' | 'HQ' | 'Jon Stahl' | 'Groundwire Labs'
-N -> 'synergy' | 'bandwidth' | 'theory of change' | 'sprint' | 'wordle' | 'bucket' | 'campaign' | 'low-hanging fruit' | 'sprint planning' | 'technology' | 'user story' | 'engagement' | 'movement as network' | 'social change' | 'data migration' | 'innovation' | 'capacity building' | 'theming' | 'advanced functionality' | 'Engagement Pyramid' | 'Gantt chart' | 'scope' | 'scrumbucket' | 'consulting' | 'engagement level' | 'utilization rate' | 'Chipotle order' | 'penguin' | 'line of sight' | 'situational awareness' | 'change management'
-P -> 'in' | 'outside' | 'to' | 'on' | 'about' | 'around'
+N -> 'theory of change' | 'sprint' | 'wordle' | 'bucket' | 'campaign' | 'user story' | 'Engagement Pyramid' | 'Gantt chart' | 'scope' | 'scrumbucket' | 'engagement level' | 'utilization rate' | 'Chipotle order' | 'penguin' | 'line of sight'
+N_collective -> 'analytics' | 'synergy' | 'bandwidth' | 'low-hanging fruit' | 'sprint planning' | 'technology' | 'engagement' | 'movement as network' | 'social change' | 'data migration' | 'innovation' | 'capacity building' | 'theming' | 'advanced functionality' | 'consulting' | 'situational awareness' | 'change management'
+P -> 'in' | 'outside' | 'to' | 'on' | 'about' | 'around' | 'of' | 'less than' | 'more than'
 SubConj -> 'although' | 'because' | 'while' | 'after' | 'as' | 'before' | 'if' | 'as long as' | 'since' | 'though' | 'unless' | 'whenever'
 ''')
 
